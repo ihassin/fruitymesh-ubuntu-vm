aea2ead123aad547ebbbd3f9de52567b206d3b4f
Itamar Hassin <ihassin@mac.com>
2016-03-01 18:40:36 -0500

    Adds serverspec for the symlinks

--- infra/spec/fruity-vb/basic_spec.rb
+++ infra/spec/fruity-vb/basic_spec.rb
@@ -15,12 +15,24 @@ describe file('/home/deploy/nrf') do
   it { should be_directory }
 end
 
-# We have the NRF51 SDK
+# We have the SDK directory
 describe file('/home/deploy/nrf/sdk') do
   it { should be_directory }
 end
+
+# We have the NRF51 SDK symlink
+describe file('/home/deploy/nrf/sdk/nrf51_sdk_latest') do
+  it { should be_symlink }
+end
+
+# We have the SDK52 directory
+describe file('/home/deploy/nrf/sdk/nrf52_sdk_latest') do
+  it { should be_symlink }
+end
+
 
 # We have the FruityMesh project
 describe file('/home/deploy/nrf/projects/fruitymesh') do
   it { should be_directory }
 end
+
